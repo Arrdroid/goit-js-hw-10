a997311fd60b49afadb84db39949be952807bb22
Arrdroid <alexmirensko@gmail.com>
2023-08-14 16:57:28 +0300

    summary

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -2,36 +2,31 @@ import axios from "axios";
 import SlimSelect from 'slim-select'
 
 axios.defaults.headers.common["x-api-key"] = "live_a7nkCJOI4lOXcdOz3Nuqe9HSeKQ6nrSw0vQnTtPoetJ1HHkAHaRUZxH3vqSIyFTk";
+const breedSelect = document.querySelector(".breed-select");
 
 export function fetchBreeds() {
   try {
-    const response = axios.get("https://api.thecatapi.com/v1/breeds");
-    console.log(response);
-    const breeds = response.data;
-    console.log(breeds);
-    const breedSelect = document.querySelector(".breed-select");
-
-    
-    const allTheBreeds = breeds.map((breed) => {
-      const option = document.createElement("option");
-      option.value = breed.id;
-      option.textContent = breed.name;
-      return `
+    const response = axios.get("https://api.thecatapi.com/v1/breeds")
+      .then(response => response.data)
+      .then(breeds => breeds.map((breed) => {
+        const option = document.createElement("option");
+        option.value = breed.id;
+        option.textContent = breed.name;
+        return `
       <p><strong class="text-info">Breed:</strong> ${catBreed.name}</p>
       <p class="text-info">${catBreed.description}</p>
       <p><strong class="text-info">Temperament:</strong> ${catBreed.temperament}</p>
       <img src="${catInfo.url}" alt="Cat Image" class="cat-image" />
+      
       `
-    }).join("");
+      }).join(""));
 
     breedSelect.insertAdjacentHTML("beforeend", allTheBreeds);
     
     const slim = new SlimSelect({
     select: '.breed-select',
     });
-
-
-    return breeds;
+  
   } catch (error) {
     console.error("Error fetching breeds:", error);
   }
@@ -45,7 +40,7 @@ export function fetchCatByBreed(breedId) {
     const catInfo = response.data[0];
     const catBreed = catInfo.breeds[0];
     console.log(catBreed);
-    if (catBreed = false || "") {
+    if (catBreed === false || "") {
       return alert `empty data received`;
       catDetailsContainer.innerHTML = "";
     }
