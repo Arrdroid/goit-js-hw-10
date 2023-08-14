d070e0e8f2fbebed992cb4332c809c46e207cf07
Arrdroid <alexmirensko@gmail.com>
2023-08-14 19:26:38 +0300

    summary

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,63 +1,62 @@
 import axios from "axios";
 import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css';
 
 axios.defaults.headers.common["x-api-key"] = "live_a7nkCJOI4lOXcdOz3Nuqe9HSeKQ6nrSw0vQnTtPoetJ1HHkAHaRUZxH3vqSIyFTk";
 const breedSelect = document.querySelector(".breed-select");
 
 export function fetchBreeds() {
   try {
-    const response = axios.get("https://api.thecatapi.com/v1/breeds")
+    const response = axios
+      .get('https://api.thecatapi.com/v1/breeds')
       .then(response => response.data)
-      .then(breeds => breeds.map((breed) => {
-        const option = document.createElement("option");
-        option.value = breed.id;
-        option.textContent = breed.name;
-        return `
-      <p><strong class="text-info">Breed:</strong> ${catBreed.name}</p>
-      <p class="text-info">${catBreed.description}</p>
-      <p><strong class="text-info">Temperament:</strong> ${catBreed.temperament}</p>
-      <img src="${catInfo.url}" alt="Cat Image" class="cat-image" />
-      
-      `
-      }).join(""));
+      .then(breeds => {
+        const allTheBreeds = breeds
+          .map(breed => `<option value= ${breed.id}>${breed.name}</option>`)
+          .join('');
 
-    breedSelect.insertAdjacentHTML("beforeend", allTheBreeds);
-    
-    const slim = new SlimSelect({
-    select: '.breed-select',
-    });
-  
+        breedSelect.insertAdjacentHTML('beforeend', allTheBreeds);
+
+        const slim = new SlimSelect({
+          select: '.breed-select',
+        });
+      });
   } catch (error) {
-    console.error("Error fetching breeds:", error);
+    console.error('Error fetching breeds:', error);
   }
 }
 
 export function fetchCatByBreed(breedId) {
   try {
-    const response = axios.get(
-      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-    );
-    const catInfo = response.data[0];
-    const catBreed = catInfo.breeds[0];
-    console.log(catBreed);
-    if (catBreed === false || "") {
-      return alert `empty data received`;
-      catDetailsContainer.innerHTML = "";
-    }
-    const catInfoContainer = document.querySelector(".cat-info");
-    let catDetailsContainer = document.querySelector(".cat-details");
+    axios
+      .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+      .then(response => response.data)
+      .then(data => {
+        const catInfo = data[0];
+        const catBreed = catInfo.breeds[0];
 
-  catDetailsContainer.innerHTML = `
-  <p><strong class="text-info">Breed:</strong> ${catBreed.name}</p>
-  <p class="text-info">${catBreed.description}</p>
-  <p><strong class="text-info">Temperament:</strong> ${catBreed.temperament}</p>
-  `;
-  catInfoContainer.innerHTML = `
-  <img src="${catInfo.url}" alt="Cat Image" class="cat-image" />
-  `;
+        if (!catInfo) {
+          alert("Empty data received");
+          return;
+        }
 
+        const catDetailsContainer = document.querySelector(".cat-details");
+        const catInfoContainer = document.querySelector(".cat-info");
+
+        catDetailsContainer.innerHTML = `
+          <p><strong class="text-info">Breed:</strong> ${catBreed.name}</p>
+          <p class="text-info">${catBreed.description}</p>
+          <p><strong class="text-info">Temperament:</strong> ${catBreed.temperament}</p>
+        `;
+
+        catInfoContainer.innerHTML = `
+          <img src="${catInfo.url}" alt="Cat Image" class="cat-image" />
+        `;
+      })
+      
   } catch (error) {
     console.error("Error fetching cat info:", error);
   }
 }
+
 
